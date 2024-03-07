c1f7e0d33612961d5dc6902520a209dc15cc451f
Gabriela Panqueva <80019285+PanquecaFuriosa@users.noreply.github.com>
2024-03-07 10:04:58 -0400

    Update Treap.cpp

--- Treap.cpp
+++ Treap.cpp
@@ -5,13 +5,13 @@
 using namespace std;
 
 // Definir una estructura para el nodo del treap
-typedef struct TreapNode
+typedef struct TreapNode *pTreapNode;
+struct TreapNode
 {
     int priority, size, value;
     struct TreapNode *left, *right;
 
-} TreapNode;
-typedef TreapNode *pTreapNode;
+};
 
 // Función para crear un nuevo nodo
 pTreapNode newNode(int value)
@@ -91,29 +91,27 @@ void multipleSwap(pTreapNode &root, int start, int end, int total)
     start--;
     end--; 
 
-    if (start == -1 && 2 * end >= total)
+    if ((start == -1) && (2 * end >= total))
     {
         pTreapNode root0, root1, root2;
         pTreapNode temp1, temp2;
         split(root, root0, temp1, total - end - 2);
         split(temp1, root1, root2, 2 * (end + 1) - total - 1);
-
         merge(temp2, root2, root1);
         merge(root, temp2, root0);
         return;
     }
 
+    bool out = 2 * end - start >= total;
+    int split2 = (out ? total - end - 2 : end - start - 1);
+    int split3 = (out ? 2 * end - total - 2 : end - start - 1);
     pTreapNode root0, root1, root2, root3;
     pTreapNode temp1, temp2, temp3, temp4, temp5;
-    bool out_range = 2 * end - start >= total;
-    int split2 = (out_range ? total - end - 2 : end - start - 1);
-    int split3 = (out_range ? 2 * end - total - 2 : end - start - 1);
-
+    
     split(root, root0, temp1, start);
     split(temp1, root1, temp2, split2);
     split(temp2, root2, root3, split3);
-
-    if (!out_range)
+    if (!out)
     {
         merge(temp4, root0, root2);
         merge(temp5, temp4, root1);
@@ -157,5 +155,3 @@ int main()
 {
     performSwaps(5, {{1,3}, {2,4}});
 }
-
-
